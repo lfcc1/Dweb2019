ba0c6302f2fdc0ae451109d8abf51dc04e1cff75
RyzonPT <Filipe-Cunha1@hotmail.com>
2019-11-14 09:54:38 +0000

    adiocnar route que foi removida anteriormente por engano

--- Aula9/obras/routes/api.js
+++ Aula9/obras/routes/api.js
@@ -3,6 +3,41 @@ const router = express.Router();
 
 var Obras = require('../controllers/obras')
 
+/* GET: lista de obras */
+router.get('/obras', function(req, res) {
+  var ano = req.query.ano
+  var compositor = req.query.compositor
+  var duracao = req.query.duracao
+  var periodo = req.query.periodo
+
+  if(ano){
+      Obras.listarbyAno(ano)
+      .then(dados => res.jsonp(dados))
+      .catch(erro => res.status(500).jsonp(erro))
+  }
+  else{
+    if(compositor && duracao){
+        Obras.listarByCompositorDuracao(compositor,duracao)
+        .then(dados => res.jsonp(dados))
+        .catch(erro => res.status(500).jsonp(erro))
+    }
+    else{
+      if(periodo){
+        Obras.listarbyPeriodo(periodo)
+        .then(dados => res.jsonp(dados))
+        .catch(erro => res.status(500).jsonp(erro))
+      }
+      else{
+        Obras.listar()
+        .then(dados => res.jsonp(dados))
+        .catch(erro => res.status(500).jsonp(erro))
+      }
+
+    }
+  }
+
+})
+
 /* GET: recupera a informação de uma obra */
 router.get('/obras/:id', function(req, res) {
   Obras.consultar(req.params.id)
